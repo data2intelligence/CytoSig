8e11e4b75adab49ac8f5631ec223009732a3af21
jiangp4 <peng.jiang@nih.gov>
2021-03-10 20:19:02 -0500

    Update

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-CellSig
+CellSig prediction model of cytokine signaling activity
+
+Prerequisite:  
+1, ridge_significance: https://github.com/data2intelligence/ridge_significance/releases    
+You will need numpy, gsl, gcc to install this package. Please read its README.md for details.
+
+2, pandas: You may install anaconda (https://www.anaconda.com) to include all required python packages.
+
+Install:
+python setup.py install
+
+Test:
+python -m unittest tests.prediction
+
+Usage:
+CellSig.py -i input_profile -o output_prefix -r random_count -a penalty_alpha -e generate_excel
+
+
+1, input_profile: input matrix of biological profiles. Each column is a biological condition, and each row should be a human gene symbol
+2, output_prefix: prefix of output files.
+
+
+random_count: default: 1000
+penalty_alpha: default: 10000
+generate_excel: default 0
---
+++ tests/prediction.py
@@ -0,0 +1,27 @@
+import unittest
+import os, sys
+import pathlib
+import subprocess
+
+fpath = pathlib.Path(__file__).parent.absolute()
+
+output = os.path.join(fpath, 'output')
+
+class TestPrediction(unittest.TestCase):    
+    def test_bulk(self):
+        cmd = ['CellSig.py', '-i', os.path.join(fpath, 'GSE147507.diff.gz'), '-o', output, '-e', '1']
+        
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.communicate()
+        
+        for title in ['Coef', 'StdErr', 'Zscore', 'Pvalue', 'xlsx']:
+            flag = os.path.exists(output + '.' + title)
+            
+            if not flag:
+                sys.stderr.write('Cannot find file %s\n' % (output + '.' + title))
+            else:
+                os.remove(output + '.' + title) # clear results for the next test
+            
+            self.assertTrue(flag)
+
+if __name__ == '__main__': unittest.main()
